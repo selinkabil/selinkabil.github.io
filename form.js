99af25ddce0dfa4ff3acc67cce0032ac9747b83f
Selin Kabil <137197073+selinkabil@users.noreply.github.com>
2023-07-09 05:49:24 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,18 @@
+
+  $(document).ready(function (){
+    $('.submit').click(function (event){
+    const names = document.forms[0].elements[0];
+    const email = document.forms[0].elements[1];
+    const message = document.forms[0].elements[2];
+    const invalid = document.getElementById('message');
+  
+    if (names.value === "" || email.value === "" || message.value === "") {
+      invalid.style.display = "block";
+      invalid.innerHTML = "All fields are required"
+    }
+    else{
+        invalid.style.display = "none";
+        console.log("clicked")
+    }
+  })
+  })
